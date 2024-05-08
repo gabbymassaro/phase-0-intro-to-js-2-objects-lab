6d66ebb0a31d4d66a0906ee3fbc5cfb8730ab9a5
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-08 10:05:14 -0400

    destructively update employee, passes third test

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
 
 // const updateEmployee = updateEmployeeWithKeyAndValue(employee, "Sam", "800 Park Ave");
 // console.log(updateEmployee); 
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress){
+  employee[name] = streetAddress;
+  return employee;
+};
+
+// destructivelyUpdateEmployeeWithKeyAndValue(employee, "Gabby", "2368 S. Whittmore");
+// console.log(employee); 
