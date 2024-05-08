1fc562747b104338208ad362ccd21caf20f9e21e
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-08 09:52:44 -0400

    Use spread operator to update employee non-destructively
    keep console logs for now, delete before pushing up the PR, this passes the first two tests

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your solution in this file!
+const employee = {name: 'Bob', streetAddress: '708 Anderson Ave.'};
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+  const updateEmployee = {...employee};
+  updateEmployee[name] = streetAddress;
+  return updateEmployee;
+}
+
+// console.log(employee);
+
+// const updateEmployee = updateEmployeeWithKeyAndValue(employee, "Sam", "800 Park Ave");
+// console.log(updateEmployee); 
