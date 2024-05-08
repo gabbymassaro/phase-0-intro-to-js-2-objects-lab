f14e7d05f2db0fe55b186e64fc83e084bca6b900
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-08 11:27:36 -0400

    Remove whitespace and console logs

--- index.js
+++ index.js
@@ -17,25 +17,7 @@ function deleteFromEmployeeByKey(employee, streetAddress) {
   return deleteEmployee;
 };
 
-
 function destructivelyDeleteFromEmployeeByKey(employee, streetAddress) {
   delete employee[streetAddress];
   return employee;
 };
-
-
-// ------------- console logging lives here ----------------// 
-console.log("original: ", employee);
-
-const updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'name', "cinderella");
-console.log("updated: ", updatedEmployee);
-
-const destructEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', "800 Park Ave");
-console.log("destructively update street: ", employee);
-
-
-const deleteEmployee = deleteFromEmployeeByKey(employee, 'streetAddress');
-console.log("Employee after deletion:", deleteEmployee);
-
-const destructDeleteEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'streetAddress');
-console.log("Destructively Delete Street Address: ", employee);
